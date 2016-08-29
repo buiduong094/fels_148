b80a6c672a95c46e54e28617646396bb09fc2aef
buiduong094 <bui.van.duong@framgia.com>
2016-08-29 09:41:06 +0700

    user_show_list_words

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :logged_in_user
+
   def index
     if search = params[:search]
       @categories = (Category.search_category search)
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,27 @@ User.create!(name:  "admin", email: "admin@framgia.com",
   password: "123456", is_admin: true, password_confirmation: "123456")
 
 50.times do |category|
-  name  = "mon #{category+1}"
+  name  = "Category #{category+1}"
   description = "day la mon thu #{category+1}"
   Category.create!(name:  name, description: description)
 end
 3.times do |category|
   category_id= category+1;
-  50.times do |i|
-  content = "tu thu #{i+1}, #{category+1}"
-    Word.create!(category_id:  category_id, content: content)
+  6.times do |i|
+    content = "word"
+    word = Word.create!(category_id:  category_id, content: content)
+    content_answer = "answer"
+    Answer.create!(word_id:  word.id, content: content_answer, is_correct: true)
+    content_answer = "answer"
+    Answer.create!(word_id:  word.id, content: content_answer)
+    content_answer = "answer"
+    Answer.create!(word_id:  word.id, content: content_answer)
   end
+end
+Lesson.create!(user_id: "1", category_id: "1")
+
+count=1
+3.times do |i|
+  Result.create!(lesson_id:  1, word_id: i+1, word_answer_id: count)
+  count += 3
 end
