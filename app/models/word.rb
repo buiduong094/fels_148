c7843db6652499b0b169d10d0ac3b55b7302367d
buiduong094 <bui.van.duong@framgia.com>
2016-08-29 12:20:41 +0700

    user_filter_word_by_some_conditions

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,9 +3,21 @@ class Word < ActiveRecord::Base
   has_many :answers
   has_many :results
 
-  query = "content like :search"
-  scope :search_word, -> search do
-    where query, search: "%#{search}%" if search.present?
+  QUERY_LEARNED = "content like :search and id in (select word_id
+    FROM results r INNER JOIN lessons l
+    ON r.lesson_id = l.id AND l.user_id = :user_id)"
+  QUERY_NOT_LEARNED = "content like :search and id not in (select word_id
+    FROM results r INNER JOIN lessons l
+    ON r.lesson_id = l.id AND l.user_id = :user_id)"
+
+  scope :in_category, -> category_id do
+    where category_id: category_id if category_id.present?
   end
+
+  scope :show_all, -> user_id, search {}
+  scope :learned, -> (user_id, search) {
+    where QUERY_LEARNED, user_id: user_id, search: "%#{search}%"}
+  scope :not_learned, -> (user_id, search) {
+    where QUERY_NOT_LEARNED, user_id: user_id, search: "%#{search}%"}
 
 end
