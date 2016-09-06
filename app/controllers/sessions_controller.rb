6d6bc2c0797235cec55adfb71b68fdc61639d2d3
= <duong.van.tien@framgia.com>
2016-09-06 11:41:17 +0700

    user-follow-unfollow
    remember-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
       user = User.find_by email: params[:session][:email]
       if user && user.authenticate(params[:session][:password])
         log_in user
-        redirect_to user
+        params[:session][:remember_me] == Settings.remembered ? remember(user) :
+          forget(user)
+        redirect_back_or user
       else
         flash.now[:danger] = t "page.invalid_email"
         render :new
