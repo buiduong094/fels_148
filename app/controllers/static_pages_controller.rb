2543472bcf332adc91faf4376ac6f39913e852cd
buiduong094 <bui.van.duong@framgia.com>
2016-09-09 16:37:50 +0700

    fix_bug_1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+      @user = current_user
       @activities = Activity.activity_in_user(current_user.id)
-       .order("created_at DESC").paginate page: params[:page],
-         per_page: Settings.activities.per_page
+        .order("created_at DESC").paginate page: params[:page],
+          per_page: Settings.activities.per_page
+      respond_to do |format|
+        format.html
+        format.json {render json: {
+          content: render_to_string({
+            partial: "shared/activities", formats: "html", layout: false
+          })
+        }}
+      end
     end
   end
 end
